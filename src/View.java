62ec8c7af049aa11eda2d1aca90dea5afbf6762a
mahdi <60757625+m-hejrati@users.noreply.github.com>
2020-05-08 19:43:05 +0430

    Design the general structure of left and center part of app

--- src/View.java
+++ src/View.java
@@ -1,4 +1,7 @@
+import javax.imageio.plugins.tiff.GeoTIFFTagSet;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,41 +11,108 @@ public class View {
 
     private JFrame mainFrame;
     private JSplitPane mainPane;
-    private JPanel left;
-    private JPanel center;
-    private JPanel right;
+    private JPanel leftPanel;
+    private JPanel centerPanel;
+    private JPanel rightPanel;
     private JMenuBar menuBar;
+    private JTree tree;
 
     public View (){
 
         mainFrame = new JFrame();
         mainFrame.setTitle("Insomnia");
         mainFrame.setSize(1100, 560);
+        mainFrame.setSize(1100, 560);
         mainFrame.setLocation(210, 170);
 //        mainFrame.setMinimumSize(new Dimension(380, 350));
         mainFrame.setResizable(true);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //------------------------------------------------------------------------------------------------------------
 
 
-        left = new JPanel();
-        left.setBackground(new Color(30, 30, 30));
-        center = new JPanel();
-        center.setBackground(new Color(30, 30, 30));
-        right = new JPanel();
-        right.setBackground(new Color(30, 30, 30));
+        //------------------------------------------------------------------------------------------------------------
+        leftPanel = new JPanel();
+//        leftPanel.setBackground(new Color(30, 30, 30));
+        JScrollPane leftScrollPane = new JScrollPane(leftPanel);
+        centerPanel = new JPanel(new BorderLayout(5, 10));
+        centerPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+//        centerPanel.setBackground(new Color(30, 30, 30));
+        JScrollPane centerScrollPane = new JScrollPane(centerPanel);
+        rightPanel = new JPanel();
+//        rightPanel.setBackground(new Color(30, 30, 30));
+        JScrollPane rightScrollPane = new JScrollPane(rightPanel);
 
-
-        JSplitPane spRight = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, center, right);
+        JSplitPane spRight = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, centerScrollPane, rightScrollPane);
         spRight.setDividerLocation(mainFrame.getSize().width / 3);
         spRight.setDividerSize(3);
 
-        mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, left, spRight);
+        mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftScrollPane, spRight);
         mainPane.setDividerSize(2);
         mainPane.setDividerLocation(mainFrame.getSize().width / 4);
         mainFrame.add(mainPane, BorderLayout.CENTER);
 
+        //------------------------------------------------------------------------------------------------------------
+        DefaultMutableTreeNode request = new DefaultMutableTreeNode("Request List");
+        tree = new JTree(request);
+//        jt.setBackground(new Color(150,150,150));
+        DefaultMutableTreeNode first = new DefaultMutableTreeNode("first group");
+        DefaultMutableTreeNode second = new DefaultMutableTreeNode("second group");
+        request.add(first);
+        request.add(second);
+        DefaultMutableTreeNode first1 = new DefaultMutableTreeNode("first req");
+        DefaultMutableTreeNode first2 = new DefaultMutableTreeNode("second req");
+        first.add(first1);
+        first.add(first2);
+        DefaultMutableTreeNode second1 = new DefaultMutableTreeNode("first req");
+        second.add(second1);
+        tree.expandRow(0);
+        leftPanel.add(tree);
+        //------------------------------------------------------------------------------------------------------------
 
+        //------------------------------------------------------------------------------------------------------------
+        JPanel northCenter = new JPanel(new BorderLayout(5,5));
+        centerPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        String URLOptions[] = {"PATCH","PUT","POST","DELETE","GET"};
+        JComboBox list = new JComboBox(URLOptions);
+        northCenter.add(list, BorderLayout.WEST);
+        northCenter.add(new JTextField(), BorderLayout.CENTER);
+        northCenter.add(new JButton("Send"), BorderLayout.EAST);
+        centerPanel.add(northCenter, BorderLayout.NORTH);
+        //------------------------------------------------------------------------------------------------------------
 
+        JTabbedPane centerCenter = new JTabbedPane();
+
+//        String bodyOptions[] = {"Form Data","JSON","Binary Data"};
+//        JComboBox boyList = new JComboBox(bodyOptions);
+        centerCenter.add("bodyList", new JPanel());
+
+        JPanel textPanel = new JPanel(new GridLayout(1,2));
+        textPanel.setBorder(new EmptyBorder(5,0,0,0));
+        textPanel.add(new JTextField());
+        textPanel.add(new JTextField());
+        JPanel checkPanel = new JPanel(new GridLayout(1,2));
+        checkPanel.setBorder(new EmptyBorder(5,0,0,0));
+        checkPanel.add(new JCheckBox());
+        checkPanel.add(new Button("*"));
+
+        JPanel keyValuePanel = new JPanel(new BorderLayout(5, 5));
+        keyValuePanel.add(textPanel, BorderLayout.CENTER);
+        keyValuePanel.add(checkPanel, BorderLayout.EAST);
+
+        JPanel boxHeaderPanel = new JPanel();
+        boxHeaderPanel.setLayout(new BoxLayout(boxHeaderPanel, BoxLayout.Y_AXIS));
+        boxHeaderPanel.add(keyValuePanel);
+
+        JPanel headerPanel = new JPanel(new BorderLayout(5,5));
+        headerPanel.add(boxHeaderPanel, BorderLayout.NORTH);
+
+        centerCenter.add("Header", headerPanel);
+        centerCenter.add("Query", new JPanel());
+        centerCenter.add("Auth", new JPanel());
+
+        centerPanel.add(centerCenter, BorderLayout.CENTER);
+
+        //------------------------------------------------------------------------------------------------------------
         menuBar = new JMenuBar();
 
         JMenu appMenu = new JMenu("Application");
@@ -59,12 +129,7 @@ public class View {
         JMenu viewMenu = new JMenu("View");
         viewMenu.setMnemonic('V');
         JMenuItem sidebarItem = new JMenuItem("Toggle Sidebar");
-        sidebarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
-        sidebarItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ev) {
-
-            }
-        });        JMenuItem fullScreenItem = new JMenuItem("Toggle Full Screen");
+        JMenuItem fullScreenItem = new JMenuItem("Toggle Full Screen");
         fullScreenItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
         fullScreenItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
@@ -95,21 +160,18 @@ public class View {
             }
         });
 
-
         appMenu.add(optionItem);
         appMenu.add(exitItem);
-
         viewMenu.add(sidebarItem);
         viewMenu.add(fullScreenItem);
-
         helpMenu.add(aboutItem);
         helpMenu.add(helpItem);
-
         menuBar.add(appMenu);
         menuBar.add(viewMenu);
         menuBar.add(helpMenu);
-
         mainFrame.setJMenuBar(menuBar);
+        //-------------------------------------------------------------------------------
+
     }
 
 
